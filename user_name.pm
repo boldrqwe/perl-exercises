096c385e3d7fa44e157f521d0c750cae19b53479
boldrqwe <boldr@mail.ru>
2024-02-14 11:18:09 +0300

    argv_env_2exc

---
+++ user_name.pm
@@ -0,0 +1,36 @@
+#! usr/bin/perl
+
+
+use strict;
+use warnings;
+
+
+my @users_list = ( "Mike", "Jack", "Kek" );
+
+# foreach my $arg (@ARGV) {
+#     print "argument: $arg\n";
+# }
+
+print "Enter name: ";
+my $user = %ENV{user_name};
+my $user_passwd = %ENV{user_passwd};
+
+chomp($user);
+
+my $find = 0;
+
+
+for my $user_name(@users_list){
+    if ( $user eq $user_name ) {
+        $find = 1;
+        last;
+    }
+}
+
+if ( $find == 1 ) {
+    print "Добро пожаловать, $user !\n Твой пароль $user_passwd";
+} else {
+    print "Ты кто такой, $user?!\n";
+}
+
+
