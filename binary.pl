ade1ee9209e7b1cf6d12de49b3ad4d98f204093c
boldrqwe <boldr@mail.ru>
2024-02-14 14:39:22 +0300

    fix

--- binary.pl
+++ binary.pl
@@ -1,13 +1,12 @@
 #!/usr/bin/perl
 use strict;
 use warnings FATAL => 'all';
-use Scalar::Util 'looks_like_number';
 
 print('enter number to find: ');
 my $number = <STDIN>;
 chomp($number);
 
-if ( !looks_like_number($number) ) {
+if ( $number !~ /^-?\d+(\.\d+)?$/ ) {
     die( 'it`s not number' );
 }
 
@@ -24,24 +23,33 @@ for ( my $i = 0; $i < @arr; $i++ ) {
 }
 
 my $first_index = 0;
-my $last_index = $#arr;
+my $last_index = scalar(@arr);
 
 my $result = binary_search( $first_index, $last_index, $number );
 
 if ( defined($result) ) {
-    print("found $result")
+    print("found $result");
 }
 else {
-    print("$number not found")
+    print("$number not found");
 }
 
 sub binary_search {
     my ($first_i, $last_i, $number_to_found) = @_;
-    my $middle = ($first_i + $last_i) / 2;
 
-    if ( $middle < 1 ) {
+    if ( $last_i == -1 ) {
         return undef;
     }
+
+    if ( $last_i == 1 ) {
+        if ( $number_to_found == $arr[1] ){
+            return $number_to_found;
+        }
+        return undef;
+    }
+
+    my $middle = ($first_i + $last_i) / 2;
+
     if ( $first_i >= $last_i || $last_i <= $first_i ) {
         return undef;
     }
@@ -51,7 +59,7 @@ sub binary_search {
     }
 
     if ( $arr[$middle] > $number_to_found ) {
-        return (binary_search($first_i, $middle, $number_to_found))
+        return (binary_search($first_i, $middle, $number_to_found));
     }
     else {
         return (binary_search($middle, $last_i, $number_to_found));
