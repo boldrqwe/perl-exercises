ebf6580c9389136eb52cceed0ff4bec2abb30fb1
boldrqwe <boldr@mail.ru>
2024-02-26 12:33:02 +0300

    check_pass16exc

--- lib/tools.pm
+++ lib/tools.pm
@@ -16,17 +16,21 @@ sub login {
 
 }
 sub reg_user {
-    my ($hash1_ref, $hash2_ref, $path) = @_;  # Получаем ссылки на хеши
-    my %new_user_password = %$hash1_ref;  # Разыменование ссылки для получения хеша
+    my ( $hash1_ref, $hash2_ref, $path ) = @_;
+    my %new_user_and_password = %$hash1_ref;
     my %config = %$hash2_ref;
 
-    my @arr = keys( %new_user_password );
+    my @arr = keys( %new_user_and_password );
     my $new_user = $arr[0];
     my $existing_user = $config{$new_user};
     if ( defined( $existing_user ) ) {
         die 'такой пользователь уже зарегистрирован'
     }
-    add_line_to_file( $path, "$new_user=$new_user_password{$new_user}" )
+    _check_user_name( keys( %new_user_and_password ));
+    my $password = $new_user_and_password{$new_user};
+    _check_user_passwd( $password );
+
+    add_line_to_file( $path, "$new_user=$password" )
 }
 
 sub add_line_to_file {
@@ -70,4 +74,18 @@ sub get_hash {
     return %hash;
 }
 
+sub _check_user_name {
+    my ( $username ) = @_;
+    return $username =~ /^[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9]$/ && $username !~ /[А-Яа-я]/ ? 1 : 0
+}
+
+sub _check_user_passwd {
+    my ( $passwd ) = @_;
+    die "Пароль должен быть не менее 8 символов\n" if length( $passwd ) < 8;
+    die "Пароль должен начинаться с латинской буквы\n" unless $passwd =~ /^[a-zA-Z]/;
+    die "Пароль должен содержать минимум один спецсимвол (!@#\$%^&*())\n" unless $passwd =~ /[!@#\$%^&*()]/;
+    die "Пароль должен содержать минимум один символ в верхнем регистре\n" unless $passwd =~ /[A-Z]/;
+    die "Пароль должен содержать минимум одну цифру\n" unless $passwd =~ /\d/;
+    return 1;
+}
 1;
